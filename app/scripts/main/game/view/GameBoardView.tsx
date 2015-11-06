55d4764e7b2c150ffc77545860c17d234d8a21ac
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2015-11-07 00:46:20 +0100

    Cell click handling

--- app/scripts/main/game/view/GameBoardView.tsx
+++ app/scripts/main/game/view/GameBoardView.tsx
@@ -15,10 +15,15 @@ namespace game {
             this.state = new GameBoardViewState();
         }
 
+        cellClicked(x: number, y:number) {
+            console.log("clicked " + x + " "+y);
+            //this.setState(new TasksViewState(this.state.count + 1));
+        }
+
         render() {
             const rows = this.props.board.map((cells:Immutable.List<CellState>, y: number) => {
                 const cellsElements = cells.map((cell: CellState, x: number) => {
-                    return (<div className="boardCell" key={x}></div>)
+                    return (<div className="boardCell" key={x} onClick={this.cellClicked.bind(this, x, y)}></div>)
                 });
                 return (<div className="boardRow" key={y}>{cellsElements}</div>)
             });
@@ -28,6 +33,5 @@ namespace game {
             )
         }
     }
-
 
 }
