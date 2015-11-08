c30bf2c9421cfb4d2221e3543bf1e15fa99da0a1
Marcin Pieciukiewicz <m.pieciukiewicz@gmail.com>
2015-11-08 19:59:31 +0100

    Game refactorized

---
+++ app/scripts/main/game/model/GameState.ts
@@ -0,0 +1,56 @@
+namespace game {
+
+    const GAME_BOARD_SIZE = 10;
+    export const enum CellState {empty, miss, ship, hit}
+
+    export const enum GamePhase {initPlayerBoard, waitForSecondPlayer, playerTurn, otherPlayerTurn, gameEnded}
+
+    export type Board = Immutable.List<Immutable.List<CellState>>;
+
+    const EMPTY_BOARD = createEmptyBoard();
+    function createEmptyBoard():Board {
+        return Immutable.Range(0, GAME_BOARD_SIZE).map(y =>
+            Immutable.Range(0, GAME_BOARD_SIZE).map(x =>
+                CellState.empty).toList()
+        ).toList();
+    }
+
+
+
+    export class GameState extends Immutable.Record({
+        gamePhase:undefined,
+        playerBoard:undefined,
+        opponentBoard:undefined}) {
+
+        static gamePhase = "gamePhase";
+        static playerBoard = "playerBoard";
+        static opponentBoard = "opponentBoard";
+
+        gamePhase: GamePhase;
+        playerBoard: Board;
+        opponentBoard: Board;
+
+        static initial = new GameState().init(GamePhase.initPlayerBoard, EMPTY_BOARD, EMPTY_BOARD);
+
+        init(gamePhase: GamePhase, playerBoard: Board, opponentBoard: Board): GameState {
+            return <GameState><any>this.merge({
+                gamePhase:gamePhase,
+                playerBoard:playerBoard,
+                opponentBoard:opponentBoard});
+        }
+
+        setIn(keyPath: Array<any>, value: any): GameState;
+        setIn(keyPath: Immutable.Iterable<any, any>, value: any): GameState;
+        setIn(keyPath: any, value: any): GameState {
+            return <GameState><any>super.setIn(keyPath, value);
+        }
+
+        set(key: string, value: any): GameState {
+            return <GameState><any>super.set(key, value);
+        }
+
+
+    }
+
+
+}
